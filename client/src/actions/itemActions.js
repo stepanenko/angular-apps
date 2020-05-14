6650a18003294cc4681db66666b9e709b52b4bc8
stepanenko <serhiy.stepanenko@aiesec.net>
2020-05-14 23:55:03 +0300

    implemented delete from mLab

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -21,11 +21,13 @@ export const addItem = item => dispatch => {
   });
 };
 
-export const deleteItem = (id) => {
-  return {
-    type: DELETE_ITEM,
-    payload: id
-  };
+export const deleteItem = id => dispatch => {
+  axios.delete(`/api/items/${id}`).then(res => {
+    dispatch({
+      type: DELETE_ITEM,
+      payload: id
+    });
+  });
 };
 
 export const setItemsLoading = () => {
