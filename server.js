c844a3121133355360fe9673489cf6b6fe8c674f
stepanenko <serhiy.stepanenko@aiesec.net>
2020-05-15 22:07:56 +0300

    deploying to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const itemRoutes = require('./routes/api/items');
 
@@ -22,6 +23,15 @@ mongoose.connect(db, {
 app.use('/api/items', itemRoutes);
 // app.use('/api/users', userRoutes);
 // app.use('/api/auth', authRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
